e56bb699a0653380e0bb442956a94cecf2799ce0
Camila Evangelista <94421173+camilaeva1@users.noreply.github.com>
2022-11-09 11:43:10 -0300

    Update Desafio1.py

--- Desafio1.py
+++ Desafio1.py
@@ -1,22 +1,25 @@
 # O número de testes necessários (no pior caso) é precisamente a quantidade de vezes que preciso dividir o vetor por dois até que ele tenha apenas um elemento
+from math import ceil
 
-count, ntent = 0, 0 #Contador de execuções
-k = int(input("Insira o valor de k: "))
-n = int(input("Insira o valor de n: "))
-helio = list(range(1, n + 1))
-
-q = (len(helio) + 1)//2
-
-while len(helio) > 1 and k > 0:
-  helio = helio[:q] #O Vetor recebe o novo vetor dividido
-  q = (len(helio) + 1)//2 #Calcula exatamente o meio, arredondando se o tamanho do vetor for ímpar
-  print(helio)
-  count += 1 #Sempre que executar vai adicionar ao contador
-ntent += 1  
-print(" ")
-print("Entrada\n", n, k, sep = " ")
-
-if n%2 == 0:
-  print("Saída\n", count+1)
-elif n%2 != 0:
-  print("Saída\n", count)
+n = int(input("Digite o N: "))  # QUANTIDADE DE HELIO MAXIMO DA BOMBA
+k = int(input("Digite K: "))  # NUMERO DE BALOES
+count = 0  # CONTAGEM DE TENTAVIVAS
+# FORMANDO UMA LISTA COM TODAS AS QUANTIDADES DE HELIO
+lista = list(range(1, n + 1))
+while len(lista) > 1:  # LEN(LISTA) = NÚMERO DE POSSIBILIDADES RESTANTES
+    # DIVIDINDO A LISTA EM DOIS AO MEIO PM = PRIMEIRA PARTE
+    pm = lista[0:ceil(len(lista) / 2)]
+    sm = lista[ceil(len(lista)/2):len(lista)]  # SM = SEGUNDA PARTE
+    # SE A PRIMEIRA PARTE FOR MAIOR, OU SE AS LISTAS FOREM IGUAIS, ESCOLHER A PRIMEIRA PARTE, JÁ QUE O PIOR CENÁRIO É O BALÃO ESTOURAR, POIS TEREMOS MAIS TESTES
+    if len(pm) > len(sm) or len(pm) == len(sm):
+        lista = list(pm)  # DECLARAR CLARAMENTE QUE A LISTA É A PRIMEIRA METADE
+    else:
+        lista = list(sm)
+    if k == 1:  # SE ESTIVERMOS NO ULTIMO BALAO, PRA DESCOBRIR A QUANTIDADE DE HÉLIO, TEMOS QUE TENTAR SOMENTE PARA CIMA
+        # SEM POSSIBILIDADE DE ESTOURAR, O PIOR CENÁRIO SERIA SE CONSEGUISSIMOS SOMENTE NO ULITMO NÚMERO DA LISTA, PORTANTO, SOMAMOS A QUANTIDADE DE POSSIBILIDADES AO COUNT ATUAL
+        count += len(lista)
+        break  # PODEMOS QUEBRAR O LAÇO
+    count += 1  # AO FINAL DO LAÇO ADICIONAR UM AO TESTE!!!
+    if lista == pm:
+        k -= 1  # SE A ESCOLHA FOR A PRIMEIRA LISTA, TEMOS QUE CONSIDERAR QUE O BALAO ESTOROU
+print(count)
